6f01a272a98f437a06594e8a1591c13a4137ce8f
Andrew Zabelin <a.zabelin1@yandex.ru>
2019-04-08 23:17:38 +0300

    create menu

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Находим инпут, который меняет тему на темную, а также кнопку 'загрузить еще' и модальное окно
+// (где будет запускаться видос), и все классы с видео
+const switcher = document.querySelector('#cbx'),
+      more     = document.querySelector('.more'),
+      modal    = document.querySelector('.modal'),
+      videos   = document.querySelectorAll('.videos__item');
+// Те видео, которые будут выводиться на экран
+let player;
+
+// Делаем меню. trigger - то, куда кликаем. boxBody - тело появляющегося элемента(родителя)
+// сontent - то, что будет всплывать. openClass - присваеваемый класс
+function bindSlideToggle(trigger, boxBody, content, openClass) {
+    let button = {
+        'element': document.querySelector(trigger),
+        'active': false
+    };
+
+    const box        = document.querySelector(boxBody),
+          boxContent = document.querySelector(content);
+
+    button.element.addEventListener('click', () => {
+        if (button.active === false) {    // Проверяем не активно ли меню
+            button.active = true;         // Если нет, то делаем активным
+            box.style.height = boxContent.clientHeight + 'px';
+            box.classList.add(openClass); // Активный класс для слайда
+        } else {
+            button.active = false;        // И наоборот
+            box.style.height = 0 + 'px';
+            box.classList.remove(openClass);
+        }
+    });
+}
+
+bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
