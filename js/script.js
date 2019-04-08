abf4f34c8af824b485e46d904d79a5243e2642a2
Andrew Zabelin <a.zabelin1@yandex.ru>
2019-04-08 23:59:28 +0300

    add dark theme, fix logo weight

--- js/script.js
+++ js/script.js
@@ -19,12 +19,12 @@ function bindSlideToggle(trigger, boxBody, content, openClass) {
           boxContent = document.querySelector(content);
 
     button.element.addEventListener('click', () => {
-        if (button.active === false) {    // Проверяем не активно ли меню
-            button.active = true;         // Если нет, то делаем активным
-            box.style.height = boxContent.clientHeight + 'px';
-            box.classList.add(openClass); // Активный класс для слайда
+        if (button.active === false) {                         // Проверяем не активно ли меню
+            button.active = true;                              // Если нет, то делаем активным
+            box.style.height = boxContent.clientHeight + 'px'; //Берем высоту родителя
+            box.classList.add(openClass);                      // Активный класс для слайда
         } else {
-            button.active = false;        // И наоборот
+            button.active = false;                             // И наоборот
             box.style.height = 0 + 'px';
             box.classList.remove(openClass);
         }
@@ -32,3 +32,48 @@ function bindSlideToggle(trigger, boxBody, content, openClass) {
 }
 
 bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
+
+// Переключение в темную тему
+
+function switchMode() {
+    if (night === false) {
+        night = true;
+        document.body.classList.add('night');
+
+        document.querySelectorAll('.hamburger > line').forEach(item => {
+            item.style.stroke = '#ffffff';
+        });
+
+        document.querySelectorAll('.videos__item-descr').forEach(item => {
+            item.style.color = '#ffffff';
+        });
+
+        document.querySelectorAll('.videos__item-views').forEach(item => {
+            item.style.color = '#ffffff';
+        });
+
+        document.querySelector('.header__item-descr').style.color = '#ffffff';
+        document.querySelector('.logo > img').src= 'logo/youtube_night.svg';
+    } else {
+        night = false;
+        document.body.classList.remove('night');
+        document.querySelectorAll('.hamburger > line').forEach(item => {
+            item.style.stroke = '#000000';
+        });
+        document.querySelectorAll('.videos__item-descr').forEach(item => {
+            item.style.color = '#000000';
+        });
+        document.querySelectorAll('.videos__item-views').forEach(item => {
+            item.style.color = '#000000';
+        });
+        document.querySelector('.header__item-descr').style.color = '#000000';
+        document.querySelector('.logo > img').src= 'logo/youtube.svg';
+    }
+}
+
+let night = false;
+
+switcher.addEventListener('change', () => {
+    switchMode();
+});
+
